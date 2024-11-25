ba5c71a675056d49140deb1683ef4248940eb3fb
k1ato <140940624+k1ato@users.noreply.github.com>
2024-11-25 11:33:28 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 python -m pip install argparse
 python -m pip install requests
-python -m impor importlib
-python -m import sys
-python -m import requests
-python -m import argparse
-python -m import cmd
-python -m import re
-python -m import importlib
-python -m import base64
-python -m import urllib
-python -m import nmap                       
-python -m import pymysql
-python -m import os
+python -m pip install importlib
+python -m pip install sys
+python -m pip install requests
+python -m pip install argparse
+python -m pip install cmd
+python -m pip install re
+python -m pip install base64
+python -m pip install urllib
+python -m pip install nmap                       
+python -m pip install pymysql
+python -m pip install os
 
 chmod +x flask_manager.py
 ln -sf $(pwd)/flask_manager.py /usr/local/bin/flask_manager
